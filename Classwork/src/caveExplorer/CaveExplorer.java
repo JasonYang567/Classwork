d70f98531ac9a8d34966d61bc39b20038514daf1
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-10-27 15:32:32 -0400

    updates

--- Classwork/src/caveExplorer/CaveExplorer.java
+++ Classwork/src/caveExplorer/CaveExplorer.java
@@ -8,9 +8,27 @@ public class CaveExplorer {
 	public static Scanner in; //user input
 	public static CaveRoom currentRoom; //changes based on how the user navigated
 	public static Inventory inventory; //where all objects found in caves are kept
+	private static boolean playing = true;
 	
 	public static void main(String[] args)
 	{
-		
+		in = new Scanner(System.in);
+		CaveRoom.setUpCaves();//create caves and starting room
+		inventory = new Inventory;
+		startExploring();
+	}
+	
+	public static void print(String s) {
+		System.out.println(s);//LATER: consider replacing with the more sophisticated "printMulti"
+	}
+	
+	public static void startExploring() {
+		while(playing) {
+			print(inventory.getDescription);
+			print(currentRoom.getDescription());
+			print(currentRoom.getDirections());
+			print("What would you like to do?");
+			currentRoom.interpretInput(in.nextLine());
+		}
 	}
 }

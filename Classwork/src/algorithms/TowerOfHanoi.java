bdf6dcb09c6f207192993c327293cdc6ce8fd26f
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-12-04 15:14:40 -0500

    tower of hanoi

---
+++ Classwork/src/algorithms/TowerOfHanoi.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+public class TowerOfHanoi {
+
+		private final static void main(String[] args) {
+			solve(8, "A", "B", "C");
+		}
+		
+		private static void solve(int num, String start, String helper, String end) {
+			
+		}
+}
